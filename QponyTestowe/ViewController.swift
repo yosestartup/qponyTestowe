3100b067aac233c19945fa16bba64ace88f4259e
Aleksander <yosestartup@gmail.com>
2020-03-09 17:48:33 +0100

    Initial Commit

---
+++ QponyTestowe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QponyTestowe
+//
+//  Created by Oleksandr Bambulyak on 09/03/2020.
+//  Copyright © 2020 Oleksandr Bambulyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
